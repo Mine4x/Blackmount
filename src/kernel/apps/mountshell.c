9e71ee1870a776870d430d0374ecc283f633e350
Mine4x <tiberiuszuhric@gmx.de>
2025-12-23 02:15:34 +0100

    Parsing for cd

--- src/kernel/apps/mountshell.c
+++ src/kernel/apps/mountshell.c
@@ -10,6 +10,7 @@
 
 #define LS_BUFFER_SIZE 1024
 #define MAX_ARGS 16
+#define MAX_PATH 256
 
 typedef struct {
     char *data;
@@ -31,12 +32,10 @@ static void input_grow(InputBuffer *buf, size_t new_capacity) {
         log_crit("shell", "kmalloc failed while growing input buffer");
         return;
     }
-
     if (buf->data) {
         memcpy(new_data, buf->data, (uint16_t)buf->size);
         kfree(buf->data);
     }
-
     buf->data = new_data;
     buf->capacity = new_capacity;
 }
@@ -46,7 +45,6 @@ static void input_push(InputBuffer *buf, char c) {
         size_t new_capacity = (buf->capacity == 0) ? 32 : buf->capacity * 2;
         input_grow(buf, new_capacity);
     }
-
     buf->data[buf->size++] = c;
     buf->data[buf->size] = '\0';
 }
@@ -62,14 +60,6 @@ static void input_clear(InputBuffer *buf) {
     if (buf->data) buf->data[0] = '\0';
 }
 
-void set_pwd(const char* new_dir) {
-    if (fs_is_dir(new_dir)) {
-        str_cpy(PWD, new_dir);
-    } else {
-        log_warn("cd", "given path isnt a directory");
-    }
-}
-
 typedef struct {
     char* argv[MAX_ARGS];
     int argc;
@@ -78,13 +68,10 @@ typedef struct {
 static void parse_args(char* input, Args* args) {
     args->argc = 0;
     while (*input) {
-        while (*input == ' ') input++; // skip spaces
+        while (*input == ' ') input++;
         if (*input == 0) break;
         if (args->argc >= MAX_ARGS) break;
-
         args->argv[args->argc++] = input;
-
-        // null-terminate argument
         while (*input && *input != ' ') input++;
         if (*input == ' ') {
             *input = '\0';
@@ -93,68 +80,111 @@ static void parse_args(char* input, Args* args) {
     }
 }
 
-void buildin_ls(const char* path) {
-    char buffer[LS_BUFFER_SIZE];
-    int result = get_dir_cont(path, buffer, LS_BUFFER_SIZE);
-
-    if (result < 0) {
-        log_err("shell", "ls failed on path: %s", path);
-        return;
+static void resolve_path(const char* pwd, const char* input, char* out) {
+    char temp[MAX_PATH];
+    int out_len = 0;
+    if (input[0] == '/') {
+        temp[0] = '/';
+        temp[1] = 0;
+        out_len = 1;
+    } else {
+        str_cpy(temp, pwd);
+        out_len = str_len(temp);
+        if (out_len == 0) {
+            temp[0] = '/';
+            temp[1] = 0;
+            out_len = 1;
+        }
     }
 
+    int i = 0;
+    while (input[i]) {
+        while (input[i] == '/') i++;
+        if (!input[i]) break;
+        int start = i;
+        while (input[i] && input[i] != '/') i++;
+        int len = i - start;
+        char comp[64];
+        for (int j = 0; j < len && j < 63; j++) comp[j] = input[start + j];
+        comp[len] = 0;
+
+        if (str_cmp(comp, ".") == 0) continue;
+        else if (str_cmp(comp, "..") == 0) {
+            if (out_len > 1) {
+                out_len--;
+                while (out_len > 0 && temp[out_len - 1] != '/') out_len--;
+                temp[out_len] = 0;
+            }
+        } else if (len >= 2 && comp[0] == '.' && comp[1] == '/') {
+            int k = 2;
+            while (comp[k] && out_len < MAX_PATH - 1) temp[out_len++] = comp[k++];
+            temp[out_len] = 0;
+        } else {
+            if (out_len > 1 && temp[out_len - 1] != '/') temp[out_len++] = '/';
+            else if (out_len == 0) temp[out_len++] = '/';
+            int k = 0;
+            while (comp[k] && out_len < MAX_PATH - 1) temp[out_len++] = comp[k++];
+            temp[out_len] = 0;
+        }
+    }
+
+    if (out_len == 0) {
+        temp[out_len++] = '/';
+        temp[out_len] = 0;
+    }
+    str_cpy(out, temp);
+}
+
+void set_pwd(const char* new_dir) {
+    char resolved[MAX_PATH];
+    resolve_path(PWD, new_dir, resolved);
+    if (fs_is_dir(resolved)) str_cpy(PWD, resolved);
+    else log_warn("cd", "given path isnt a directory");
+}
+
+void buildin_ls(const char* path) {
+    char buffer[LS_BUFFER_SIZE];
+    char resolved[MAX_PATH];
+    resolve_path(PWD, path, resolved);
+    int result = get_dir_cont(resolved, buffer, LS_BUFFER_SIZE);
+    if (result < 0) {
+        log_err("shell", "ls failed on path: %s", resolved);
+        return;
+    }
     int i = 0;
     while (i < result && buffer[i]) {
         char* entry = buffer + i;
         int len = 0;
-        while (buffer[i] && buffer[i] != '\n') {
-            i++;
-            len++;
-        }
-
-        for (int j = 0; j < len; j++)
-            printf("%c", entry[j]);
+        while (buffer[i] && buffer[i] != '\n') { i++; len++; }
+        for (int j = 0; j < len; j++) printf("%c", entry[j]);
         printf("\n");
-
         if (buffer[i] == '\n') i++;
     }
 }
 
 void mountshell_start() {
     keyboard_init();
-
     InputBuffer input;
     input_init(&input);
-
     printf("\x1b[36mMountshell v0.0.1\n");
     printf("\x1b[36mType 'help' or '?' for a list of commands\x1b[0m\n");
-
     printf("\x1b[0m%s> ", PWD);
-
     while (1) {
         if (!keyboard_has_input()) continue;
-
         char c = keyboard_getchar();
         if (c == 0) continue;
-
         if (c == '\n') {
             printf("\n");
-
             if (input.size > 0) {
                 Args args;
                 parse_args(input.data, &args);
-
                 if (args.argc > 0) {
                     if (str_cmp(args.argv[0], "cd") == 0) {
-                        if (args.argc > 1) {
-                            set_pwd(args.argv[1]);
-                        } else {
-                            printf("cd: missing argument\n");
-                        }
+                        if (args.argc > 1) set_pwd(args.argv[1]);
+                        else printf("cd: missing argument\n");
                     } else if (str_cmp(args.argv[0], "ls") == 0) {
-                        if (args.argc > 1)
-                            buildin_ls(args.argv[1]);
-                        else
-                            buildin_ls(PWD);
+                        if (args.argc > 1) buildin_ls(args.argv[1]);
+                        else buildin_ls(PWD);
                     } else if (str_cmp(args.argv[0], "help") == 0 || str_cmp(args.argv[0], "?") == 0) {
                         printf("Available commands: cd, ls, help, ?\n");
                     } else {
@@ -162,20 +192,14 @@ void mountshell_start() {
                     }
                 }
             }
-
             input_clear(&input);
             printf("%s> ", PWD);
             continue;
         }
-
         if (c == '\b' || c == 127) {
-            if (input.size > 0) {
-                input_pop(&input);
-                VGA_backspace();
-            }
+            if (input.size > 0) { input_pop(&input); VGA_backspace(); }
             continue;
         }
-
         input_push(&input, c);
         printf("%c", c);
     }
