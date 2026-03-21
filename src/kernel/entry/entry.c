d522bb53543c015df8e910978354debdf26cc737
Mine4x <tiberiuszuhric@gmx.de>
2026-03-21 19:24:31 +0100

    Removed test user

--- src/kernel/entry/entry.c
+++ src/kernel/entry/entry.c
@@ -138,8 +138,6 @@ void kmain(void)
 
     VFS_Create("/root", true);
     user_load_from_disk();
-    user_create_user("tib", 0);
-    user_set_password(1, "1");
     user_save_to_disk();
 
     fb_make_dev();
