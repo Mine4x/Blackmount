ee3e763650c716360a72cbc515ac42b19238b3cd
Mine4x <tiberiuszuhric@gmx.de>
2026-03-25 19:58:12 +0100

    Fixed clear command

--- src/apps/clear/main.c
+++ src/apps/clear/main.c
@@ -1,22 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
-#include <dev/console.h>
-
-#define TTY_CLEAR 1
 
 int main()
 {
-    int fd = open("/dev/tty");
-    if (fd < 0)
-    {
-        printf("Unable to open /dev/tty\n");
-        //printf("\033[2J");
-    }
-
-    //ioctl(fd, TTY_CLEAR, NULL);
-    console_clear(fd);
-
-    close(fd);
-
-    return 0;
+    printf("\033[2J"); // slow and temporary
 }
